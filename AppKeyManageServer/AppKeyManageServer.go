e11f4e4114d7c4bd9e69f21b0340b5e53b884c7b
jianying.li <lijianying12@gmail.com>
2017-05-14 15:46:03 +0800

    define obj

--- AppKeyManageServer/AppKeyManageServer.go
+++ AppKeyManageServer/AppKeyManageServer.go
@@ -1,1 +1,38 @@
 package AppKeyManageServer
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/lijianying10/api-auth/AuthKey"
+)
+
+type AppKeyManageServer struct {
+	connStr string
+	keys    map[string]AuthKey.AuthKey
+}
+
+func New(dbConn string) AppKeyManageServer {
+	return &AppKeyManageServer{
+		connStr: dbConn,
+	}
+}
+
+func (akms *AppKeyManageServer) ListenAndServe() error {
+	return nil
+}
+
+func (akms *AppKeyManageServer) HandlerNewAppKey(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+}
+func (akms *AppKeyManageServer) HandlerGetAppKeys(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+}
+func (akms *AppKeyManageServer) HandlerRefreashAppKeys(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+}
+
+func (akms *AppKeyManageServer) Refreash() error {
+	return nil
+}
+
+func (akms *AppKeyManageServer) Get(AppKey string) (AppSecret string, err error) {
+	return "", nil
+}
--- ReverseProxyServer/ReverseProxyServer.go
+++ ReverseProxyServer/ReverseProxyServer.go
@@ -1,1 +1,21 @@
 package ReverseProxyServer
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type ReverseProxyServer struct {
+	appKeyManageServer *AppKeyManageServer
+}
+
+func New(akms *AppKeyManageServer) *ReverseProxyServer {
+	return &ReverseProxyServer{
+		appKeyManageServer: akms,
+	}
+}
+
+func (rps *ReverseProxyServer) ListenAndServe(serve string) {}
+
+func (rps *ReverseProxyServer) Handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {}
